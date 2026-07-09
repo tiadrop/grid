43fe0cb590eba95740a85ef90f12c2aa90b72151
Aleta <aletabix@pm.me>
2026-07-09 20:49:12 +0100

    pathfinding test

--- test/grid.test.ts
+++ test/grid.test.ts
@@ -1,5 +1,6 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, test } from 'vitest'
 import { Grid } from '../src/grid'
+import { Pipe2D } from '@xtia/pipe2d';
 
 describe("Grid", () => {
 	it("initialises with correct values", () => {
@@ -13,5 +14,28 @@ describe("Grid", () => {
 		expect(fromInit.get(0, 0)).toBe(0);
 		expect(fromPipe.get(4, 3)).toBe(406);
 		expect(fromInit.get(4, 3)).toBe(406);
-	})
+	});
+
+	describe("Pathfinding", () => {
+		it("finds optimal path around obstacles", () => {
+			const grid = Grid.from(Pipe2D.fromFlatArrayXY([
+				1, 2, 2, 9,
+				5, 9, 2, 9,
+				1, 2, 3, 9,
+				1, 1, 1, 1
+			], 4, 4));
+			const start = grid.cells.get(0, 0);
+			const end = grid.cells.get(3, 3);
+			const path = start.findPath(end, c => c.value)!;
+			expect(path).toBeTruthy();
+			expect(path.map(c => c.value).join("")).toBe("511111");
+			// add blockage to bottom-left and try again
+			grid.set(0, 3, 4);
+			const path2 = start.findPath(end, c => c.value)!;
+			expect(path2).toBeTruthy();
+			expect(path2.map(c => c.value).join("")).toBe("222311");
+		});
+	});
+
 })
+
