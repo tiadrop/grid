29481275c123c1e1f336153d02e7e1cb058c3103
Aleta <aletabix@pm.me>
2026-07-10 18:09:36 +0100

    add tests

--- test/grid.test.ts
+++ test/grid.test.ts
@@ -1,9 +1,9 @@
-import { describe, it, expect, test } from 'vitest'
-import { Grid } from '../src/grid'
+import { describe, it, expect, test, vitest, beforeEach } from 'vitest'
+import { Cell, Grid } from '../src/grid'
 import { Pipe2D } from '@xtia/pipe2d';
 
 describe("Grid", () => {
-	it("initialises with correct values", () => {
+	it("initialises from Source2D with correct values", () => {
 		const fromPipe = Grid.from({
 			width: 10,
 			height: 15,
@@ -16,26 +16,203 @@ describe("Grid", () => {
 		expect(fromInit.get(4, 3)).toBe(406);
 	});
 
-	describe("Pathfinding", () => {
-		it("finds optimal path around obstacles", () => {
-			const grid = Grid.from(Pipe2D.fromFlatArrayXY([
+	describe("Cell operations", () => {
+		test("cell.value reads & writes source", () => {
+			const base = Grid.solid(4, 4, false);
+			const cell = base.cells.get(1, 1);
+			expect(cell.value).toBe(false);
+			cell.value = true;
+			expect(base.get(1, 1)).toBe(true);
+		});
+		test("cell.look() returns relative cell", () => {
+			const base = Grid.solid(4, 4, false);
+			base.set(1, 1, true);
+			const topLeft = base.cells.get(0, 0);
+			const inset = topLeft.look(1, 1)!;
+			expect(inset).toBeTruthy();
+			expect(inset.value).toBe(true);
+			expect(inset.look(100, 0)).not.toBeTruthy();
+		});
+		test("cells are consistently identified", () => {
+			const base = Grid.solid(4, 4, false);
+			const cell1 = base.cells.get(0, 0);
+			const cell2 = base.cells.get(0, 0);
+			expect(cell1).toBe(cell2);
+			expect(cell1.look(0, 1)?.look(0, -1)).toBe(cell1);
+		});
+
+		describe("Pathfinding", () => {
+			const pathTestData = Pipe2D.fromFlatArrayXY([
 				1, 2, 2, 9,
-				5, 9, 2, 9,
+				5, Infinity, 2, 9,
 				1, 2, 3, 9,
 				1, 1, 1, 1
-			], 4, 4));
-			const start = grid.cells.get(0, 0);
-			const end = grid.cells.get(3, 3);
-			const path = start.findPath(end, c => c.value)!;
-			expect(path).toBeTruthy();
-			expect(path.map(c => c.value).join("")).toBe("511111");
-			// add blockage to bottom-left and try again
-			grid.set(0, 3, 4);
-			const path2 = start.findPath(end, c => c.value)!;
-			expect(path2).toBeTruthy();
-			expect(path2.map(c => c.value).join("")).toBe("222311");
+			], 4, 4);
+
+			test("finds optimal path around obstacles", () => {
+				const grid = Grid.from(pathTestData);
+				const start = grid.cells.get(0, 0);
+				const end = grid.cells.get(3, 3);
+				const path = start.findPath(end, c => c.value)!;
+				expect(path).toBeTruthy();
+				expect(path.map(c => c.value).join("")).toBe("511111");
+				// add blockage to bottom-left and try again
+				grid.set(0, 3, 4);
+				const path2 = start.findPath(end, c => c.value)!;
+				expect(path2).toBeTruthy();
+				expect(path2.map(c => c.value).join("")).toBe("222311");
+			});
+
+			test("pathing to foreign cells throws", () => {
+				const base = Grid.solid(5, 5, 1);
+				const start = base.cells.get(1, 1);
+				const region = base.region(0, 0, 4, 4);
+				const target = region.cells.get(3, 3);
+				const fn = () => start.findPath(target, c => c.value);
+				expect(fn).toThrow();
+			});
+
+			test("reusable path maps", () => {
+				const grid = Grid.from(pathTestData);
+				const start = grid.cells.get(0, 0);
+				const paths = start.getPathMap(c => c.value);
+				const toCorner = paths.get(3, 3);
+				const to3 = paths.get(2, 2);
+				expect(toCorner?.map(c => c.value).join("")).toBe("511111");
+				expect(to3?.map(c => c.value).join("")).toBe("2223");
+			});
+
+			test("shortcuts", () => {
+				const grid = Grid.from(pathTestData);
+				const start = grid.cells.get(0, 0);
+				const end = grid.cells.get(3, 3);
+				const path = start.findPath(
+					end,
+					c => c.value,
+					{
+						shortcutMap: (x, y) => (x == 2 && y == 0) ? [[1, 3]] : undefined
+					}
+				);
+				expect(path?.map(c => c.value).join("")).toBe("22111");
+			});
+
 		});
+
+		describe("Visibility map", () => {
+			test("respects maxDistance", () => {
+				const grid = Grid.solid(100, 100, 0);
+				const viewer = grid.cells.get(30, 30);
+				const vis = viewer.createVisibilityMap(() => true, {
+					maxDistance: 5,
+				});
+				expect(vis.get(viewer.x + 4, viewer.y)).toBe(true);
+				expect(vis.get(viewer.x + 6, viewer.y)).toBe(false);
+			})
+		})
+
 	});
+
+	test("regions are self-bounded", () => {
+		const base = Grid.solid(10, 10, false);
+		const corner = base.region(0, 0, 3, 3);
+		const read = vitest.fn(() => corner.get(5, 0));
+		const write = vitest.fn(() => corner.set(5, 0, true));
+		expect(read).toThrow();
+		expect(write).toThrow();
+	});
+
+	test("region writes affect parents at correct offset", () => {
+		const base = Grid.solid(5, 5, 0);
+		const corner = base.region(3, 3, 2, 2);
+		corner.set(1, 1, 1);
+		expect(base.get(4, 4)).toBe(1);
+		expect(base.cells.get(4, 4).getNeighbours(true)).not.toContain(1);
+	});
+
+	test("user-supplied storage is correctly read & written", () => {
+		const store = [
+			[0, 1, 2, 3],
+			[4, 5, 6, 7],
+			[8, 9, 10, 11],
+			[12, 13, 14, 15]
+		];
+		const grid = Grid.wrap(4, 4, (x, y) => store[y][x], (x, y, v) => store[y][x] = v);
+		expect(grid.get(0, 3)).toBe(12);
+		grid.set(2, 1, 100);
+		expect(store[1][2]).toBe(100);
+	});
+
+	test("fill()", () => {
+		const base = Grid.solid(3, 3, 0);
+		base.fill(4);
+		base.region(1, 1, 2, 2).fill(5);
+		/* 444
+		   455
+		   455 */
+		expect(base.pipe.toFlatArrayXY().join("")).toBe("444455455")
+	});
+
+	describe("change event", () => {
+		test("triggers on set", () => {
+			const base = Grid.solid(4, 4, 0);
+			const handler = vitest.fn((event: any) => {
+				expect(event.changedCells.size).toBe(1);
+			});
+			base.on("change", handler);
+			expect(handler).not.toHaveBeenCalled();
+			base.set(1, 1, 5);
+			expect(handler).toHaveBeenCalled();
+		});
+		test("suspended during batchUpdate", () => {
+			const base = Grid.solid(4, 4, 0);
+			const checkCell = base.cells.get(0, 0);
+			const handler = vitest.fn((event: any) => {
+				expect(event.changedCells.size).toBe(3);
+				expect(event.changedCells.has(checkCell)).toBeTruthy();
+			});
+			base.on("change", handler);
+			expect(handler).not.toHaveBeenCalled();
+			base.batchUpdate(() => {
+				checkCell.value = 5;
+				base.set(1, 0, 5);
+				base.set(0, 1, 5);
+				expect(handler).not.toHaveBeenCalled();
+			});
+			expect(handler).toHaveBeenCalled();
+		});
+		test("don't trigger on duplicate write", () => {
+			const grid = Grid.solid(2, 2, 0);
+			const fn = vitest.fn();
+			grid.on("change", fn);
+			grid.set(0, 0, 0);
+			expect(fn).not.toHaveBeenCalled();
+		})
+	});
+
+	test("pasting", () => {
+		const base = Grid.from(Pipe2D.fromFlatArrayXY([
+			0,0,1,1,
+			0,1,1,1,
+			0,2,2,1,
+			1,2,2,3
+		], 4, 4));
+		const baseCopy = Grid.from(base);
+		const brush = Grid.from(Pipe2D.fromFlatArrayXY([
+			5,5,5,
+			6,6,6,
+			7,7,7
+		], 3, 3));
+		base.paste(brush, 1, 1);
+		expect(base.pipe.toFlatArrayXY().join("")).toBe("0011055506661777");
+		const mask = Grid.from(Pipe2D.fromFlatArrayXY([
+			1,1,0,0,
+			1,1,0,0,
+			0,0,0,0,
+			1,1,1,0
+		], 4, 4).map(n => n == 1));
+		baseCopy.paste(brush, 1, 1, mask);
+		expect(baseCopy.pipe.toFlatArrayXY().join("")).toBe("0011051102211773")
+	})
 
 })
 
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vitest } from 'vitest'
 import { OrderedQueue } from "../src/utils";
 
 describe("OrderedQueue", () => {
@@ -9,5 +9,19 @@ describe("OrderedQueue", () => {
 		expect(q.take()).toBe(2);
 		expect(q.take()).toBe(4);
 		expect(q.take()).toBe(6);
+	});
+	it("throws on empty take", () => {
+		const q = new OrderedQueue(v => v, 4);
+		const take = vitest.fn(() => q.take());
+		expect(take).not.toThrow();
+		expect(take).toThrow();
+	});
+	it("reports correct length", () => {
+		const q = new OrderedQueue(v => v, 4);
+		expect(q.length).toBe(1);
+		q.add(5);
+		expect(q.length).toBe(2);
+		q.take();
+		expect(q.length).toBe(1);
 	});
 })
